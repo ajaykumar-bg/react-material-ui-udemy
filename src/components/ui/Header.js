ece9f4e683316da9ec5f338c6405dba066c7c7cb
ajaykumar-bg <ajaykumarbg.tvm@gmail.com>
2020-10-09 14:58:57 +0530

    Moved ElevationScroll component

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 
 import { makeStyles } from '@material-ui/styles';
 
@@ -7,22 +6,8 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 
+import ElevationScroll from './ElevationScroll';
 import logo from '../../assets/logo.svg';
-
-function ElevationScroll(props) {
-	const { children } = props;
-	// Note that you normally won't need to set the window ref as useScrollTrigger
-	// will default to window.
-	// This is only being set here because the demo is in an iframe.
-	const trigger = useScrollTrigger({
-		disableHysteresis: true,
-		threshold: 0,
-	});
-
-	return React.cloneElement(children, {
-		elevation: trigger ? 4 : 0,
-	});
-}
 
 const useStyles = makeStyles((theme) => ({
 	toolbarMargin: {
